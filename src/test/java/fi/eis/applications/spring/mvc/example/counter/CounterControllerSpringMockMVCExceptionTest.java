5e9f5f9f457cd52d1ac4f9a19769b1fd6d8c984d
eis <eis@iki.fi>
2017-02-03 00:12:39 +0200

    added exception tests

---
+++ src/test/java/fi/eis/applications/spring/mvc/example/counter/CounterControllerSpringMockMVCExceptionTest.java
@@ -0,0 +1,72 @@
+package fi.eis.applications.spring.mvc.example.counter;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpStatusCodeException;
+
+import fi.eis.applications.spring.mvc.example.WebConfig;
+
+@RunWith(SpringRunner.class)
+@WebAppConfiguration 
+@ContextConfiguration(classes = { WebConfig.class })
+public class CounterControllerSpringMockMVCExceptionTest {
+
+    @Mock
+    CounterService counterService;
+
+    @InjectMocks
+    CounterController controllerUnderTest;    
+    
+    MockMvc mvc;
+    
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mvc = MockMvcBuilders.standaloneSetup(controllerUnderTest)
+                .setControllerAdvice(new CounterControllerAdvise())
+                .build();
+    }
+
+    @Test
+    public void testNPE() throws Exception {
+        Mockito.when(counterService.count(ArgumentMatchers.notNull()))
+            .thenThrow(new NullPointerException());
+        mvc.perform(
+                MockMvcRequestBuilders.post("/add")
+                    .accept(MediaType.APPLICATION_JSON)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("{\"int1\":1, \"int2\":2}"))
+            .andExpect(status().isInternalServerError())
+            .andExpect(content().string("{\"message\":\"internal_error\"}"));
+    }
+    @Test
+    public void testHTTPException() throws Exception {
+        Mockito.when(counterService.count(ArgumentMatchers.notNull()))
+            .thenThrow(new HttpClientErrorException(HttpStatus.BAD_REQUEST));
+        mvc.perform(
+                MockMvcRequestBuilders.post("/add")
+                    .accept(MediaType.APPLICATION_JSON)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("{\"int1\":1, \"int2\":2}"))
+            .andExpect(status().isBadRequest())
+            .andExpect(content().string("{\"message\":\"BAD_REQUEST\"}"));
+    }
+}
